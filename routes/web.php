aa091a256f3a222b29c03ed9a61145f44eaecdc6
denbich <denis.bichler9@gmail.com>
2021-04-11 19:42:24 +0200

    Update web.php
    nauka 11.04, koniec 84 strona

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello/{name}/{age}', function (string $name, int $age) {
+
+    echo "Cześć ". $name . ", masz ". $age . " lat.";
+
+});
+
+Route::get('/hello/{name?}/{age?}', function (string $name = "nieznajomy", int $age = null) {
+
+    echo "Witaj ".$name.", ";
+    if (is_null($age))
+    {
+        echo "Nie podałeś wieku!";
+    } else {
+        echo "Masz już ".$age." lat.";
+    }
+
+})->name('Powitanie');
